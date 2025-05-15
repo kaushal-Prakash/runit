0502c667fcf117bea15162a20b6540df525bf969
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2025-05-15 23:18:48 +0530

    initial commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "RunIt",
+  description: "A collaborative code execution platform that allows developers to write, edit, and instantly run code in a sandboxed environment.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:title"
+          content="RunIt | write, edit, and run code in seconds"
+        />
+        <meta
+          property="og:description"
+          content="A collaborative code execution platform that allows developers to write, edit, and instantly run code in a sandboxed environment."
+        />
+        <meta property="og:url" content="" />
+      </head>
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
